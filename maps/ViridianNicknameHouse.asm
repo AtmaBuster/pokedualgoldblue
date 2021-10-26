36ecb72774adb421b1bdade79d8bd37b4e7c2ff3
Atma <ultiomos@gmail.com>
2021-10-26 13:56:43 -0500

    Added events to ViridianNicknameHouse

--- maps/ViridianNicknameHouse.asm
+++ maps/ViridianNicknameHouse.asm
@@ -9,6 +9,45 @@ ViridianNicknameHouse_MapScripts:
 
 	def_callbacks
 
+ViridianNicknameHouse_ManScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "Coming up with"
+	line "nicknames is fun,"
+	cont "but hard."
+
+	para "Simple names are"
+	line "the easiest to"
+	cont "remember."
+	done
+
+ViridianNicknameHouse_GirlScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "My Daddy loves"
+	line "#MON too."
+	done
+
+ViridianNicknameHouse_SpearowScript:
+	faceplayer
+	opentext
+	writetext .Text
+	cry SPEAROW
+	waitbutton
+	closetext
+	end
+
+.Text:
+	text "SPEARY: Tetweet!"
+	done
+
+ViridianNicknameHouse_ClipboardScript:
+	jumptext .Text
+.Text:
+	text "SPEAROW"
+	line "Name: SPEARY"
+	done
+
 ViridianNicknameHouse_MapEvents:
 	db 0, 0 ; filler
 
@@ -21,7 +60,7 @@ ViridianNicknameHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  5,  3, SPRITE_BALDING_GUY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  1,  4, SPRITE_LITTLE_GIRL, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 4, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  5,  5, SPRITE_KANTO_BIRD, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 4, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  4,  0, SPRITE_CLIPBOARD, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  5,  3, SPRITE_BALDING_GUY, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ViridianNicknameHouse_ManScript, -1
+	object_event  1,  4, SPRITE_LITTLE_GIRL, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 4, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ViridianNicknameHouse_GirlScript, -1
+	object_event  5,  5, SPRITE_KANTO_BIRD, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 4, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ViridianNicknameHouse_SpearowScript, -1
+	object_event  4,  0, SPRITE_CLIPBOARD, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ViridianNicknameHouse_ClipboardScript, -1
