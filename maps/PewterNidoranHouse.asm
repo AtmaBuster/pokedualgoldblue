76b044ed28366c9ee19d2a76672a4149f9be6ad6
Atma <ultiomos@gmail.com>
2021-10-28 12:25:57 -0500

    Added events to PewterNidoranHouse

--- maps/PewterNidoranHouse.asm
+++ maps/PewterNidoranHouse.asm
@@ -8,6 +8,46 @@ PewterNidoranHouse_MapScripts:
 
 	def_callbacks
 
+PewterNidoranHouse_NidoranScript:
+	faceplayer
+	opentext
+	writetext .Text
+	cry NIDORAN_M
+	waitbutton
+	closetext
+	end
+
+.Text:
+	text "NIDORAN: Bowbow!"
+	done
+
+PewterNidoranHouse_BoyScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "NIDORAN sit!"
+	done
+
+PewterNidoranHouse_GuyScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "Our #MON's an"
+	line "outsider, so it's"
+	cont "hard to handle."
+
+	para "An outsider is a"
+	line "#MON that you"
+	cont "get in a trade."
+
+	para "It grows fast, but"
+	line "it may ignore an"
+	cont "unskilled trainer"
+	cont "in battle!"
+
+	para "If only we had"
+	line "some BADGEs..."
+	done
+
+
 PewterNidoranHouse_MapEvents:
 	db 0, 0 ; filler
 
@@ -20,6 +60,6 @@ PewterNidoranHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  4,  5, SPRITE_MONSTER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  3,  5, SPRITE_LITTLE_BOY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  1,  2, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  4,  5, SPRITE_MONSTER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterNidoranHouse_NidoranScript, -1
+	object_event  3,  5, SPRITE_LITTLE_BOY, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterNidoranHouse_BoyScript, -1
+	object_event  1,  2, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterNidoranHouse_GuyScript, -1
