ab9d862106799b42ffa7abbf63950e2ba4c2d57e
Atma <ultiomos@gmail.com>
2021-12-09 16:58:34 -0600

    Added events to MtMoon1F

--- maps/MtMoon1F.asm
+++ maps/MtMoon1F.asm
@@ -18,6 +18,220 @@ MtMoon1F_MapScripts:
 
 	def_callbacks
 
+TrainerHikerMarcos:
+	trainer HIKER_GEN1, MARCOS_GEN1, EVENT_BEAT_HIKER_MARCOS_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "WHOA! You shocked"
+	line "me! Oh, you're"
+	cont "just a kid!"
+	done
+
+.BeatenText:
+	text "Wow!"
+	line "Shocked again!"
+	done
+
+.AfterText:
+	text "Kids like you"
+	line "shouldn't be"
+	cont "here!"
+	done
+
+TrainerYoungsterJosh:
+	trainer YOUNGSTER_GEN1, JOSH_GEN1, EVENT_BEAT_YOUNGSTER_JOSH_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "Did you come to"
+	line "explore too?"
+	done
+
+.BeatenText:
+	text "Losing"
+	line "stinks!"
+	done
+
+.AfterText:
+	text "I came down here"
+	line "to show off to"
+	cont "girls."
+	done
+
+TrainerLassMiriam:
+	trainer LASS_GEN1, MIRIAM_GEN1, EVENT_BEAT_LASS_MIRIAM_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "Wow! It's way"
+	line "bigger in here"
+	cont "than I thought!"
+	done
+
+.BeatenText:
+	text "Oh!"
+	line "I lost it!"
+	done
+
+.AfterText:
+	text "How do you get"
+	line "out of here?"
+	done
+
+TrainerSuperNerdJovan:
+	trainer SUPER_NERD_GEN1, JOVAN_GEN1, EVENT_BEAT_SUPER_NERD_JOVAN_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "What! Don't sneak"
+	line "up on me!"
+	done
+
+.BeatenText:
+	text "My"
+	line "#MON won't do!"
+	done
+
+.AfterText:
+	text "I have to find"
+	line "stronger #MON."
+	done
+
+TrainerLassIris:
+	trainer LASS_GEN1, IRIS_GEN1, EVENT_BEAT_LASS_IRIS_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "What? I'm waiting"
+	line "for my friends to"
+	cont "find me here."
+	done
+
+.BeatenText:
+	text "I lost?"
+	done
+
+.AfterText:
+	text "I heard there are"
+	line "some very rare"
+	cont "fossils here."
+	done
+
+TrainerBugCatcherKent:
+	trainer BUG_CATCHER_GEN1, KENT_GEN1, EVENT_BEAT_BUG_CATCHER_KENT_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "Suspicious men"
+	line "are in the cave."
+	cont "What about you?"
+	done
+
+.BeatenText:
+	text "You"
+	line "got me!"
+	done
+
+.AfterText:
+	text "I saw them! I'm"
+	line "sure they're from"
+	cont "TEAM ROCKET!"
+	done
+
+TrainerBugCatcherRobby:
+	trainer BUG_CATCHER_GEN1, ROBBY_GEN1, EVENT_BEAT_BUG_CATCHER_ROBBY_GEN1, .SeenText, .BeatenText, 0, .Script
+.Script:
+	endifjustbattled
+	opentext
+	writetext .AfterText
+	waitbutton
+	closetext
+	end
+
+.SeenText:
+	text "Go through this"
+	line "cave to get to"
+	cont "CERULEAN CITY!"
+	done
+
+.BeatenText:
+	text "I"
+	line "lost."
+	done
+
+.AfterText:
+	text "ZUBAT is tough!"
+	line "But, it can be"
+	cont "useful if you"
+	cont "catch one."
+	done
+
+MtMoon1F_Potion1Script:
+	itemball POTION
+
+MtMoon1F_MoonStoneScript:
+	itemball MOON_STONE
+
+MtMoon1F_RareCandyScript:
+	itemball RARE_CANDY
+
+MtMoon1F_EscapeRopeScript:
+	itemball ESCAPE_ROPE
+
+MtMoon1F_Potion2Script:
+	itemball POTION
+
+MtMoon1F_TMWaterGunScript:
+IF DEF(_GOLD)
+	itemball TM_DYNAMICPUNCH
+ELIF DEF(_BLUE)
+	itemball TM_WATER_GUN
+ENDC
+
+MtMoon1FSignScript:
+	jumptext .Text
+.Text:
+	text "Beware! ZUBAT is"
+	line "a blood sucker!"
+	done
+
 MtMoon1F_MapEvents:
 	db 0, 0 ; filler
 
@@ -31,19 +245,19 @@ MtMoon1F_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event 15, 23, BGEVENT_READ, BGEvent
+	bg_event 15, 23, BGEVENT_READ, MtMoon1FSignScript
 
 	def_object_events
-	object_event  5,  6, SPRITE_HIKER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event 12, 16, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event 30,  4, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event 24, 31, SPRITE_KANTO_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event 16, 23, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event  7, 22, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event 30, 27, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
-	object_event  2, 20, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  2,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event 35, 31, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event 36, 23, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event 20, 33, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  5, 32, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  5,  6, SPRITE_HIKER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerHikerMarcos, -1
+	object_event 12, 16, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerYoungsterJosh, -1
+	object_event 30,  4, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerLassMiriam, -1
+	object_event 24, 31, SPRITE_KANTO_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerSuperNerdJovan, -1
+	object_event 16, 23, SPRITE_COOLTRAINER_F, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerLassIris, -1
+	object_event  7, 22, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerBugCatcherKent, -1
+	object_event 30, 27, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerBugCatcherRobby, -1
+	object_event  2, 20, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MtMoon1F_Potion1Script, EVENT_MT_MOON_1F_POTION1
+	object_event  2,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MtMoon1F_MoonStoneScript, EVENT_MT_MOON_1F_MOON_STONE
+	object_event 35, 31, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MtMoon1F_RareCandyScript, EVENT_MT_MOON_1F_RARE_CANDY
+	object_event 36, 23, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MtMoon1F_EscapeRopeScript, EVENT_MT_MOON_1F_ESCAPE_ROPE
+	object_event 20, 33, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MtMoon1F_Potion2Script, EVENT_MT_MOON_1F_POTION2
+	object_event  5, 32, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, MtMoon1F_TMWaterGunScript, EVENT_MT_MOON_1F_TM_WATER_GUN
