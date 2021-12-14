06a58cb13d463c945b244800bcd0f2f83bcd51dc
Atma <ultiomos@gmail.com>
2021-12-14 15:53:21 -0600

    Added events to Route18Gate2F

--- maps/Route18Gate2F.asm
+++ maps/Route18Gate2F.asm
@@ -6,6 +6,34 @@ Route18Gate2F_MapScripts:
 
 	def_callbacks
 
+Route18Gate2F_HadenScript:
+	faceplayer
+	opentext
+	trade NPC_TRADE_HADEN
+	waitbutton
+	closetext
+	end
+
+Route18Gate2F_BinocularsLeftScript:
+	jumptext .Text
+.Text:
+	text "Looked into the"
+	line "binoculars."
+
+	para "PALLET TOWN is in"
+	line "the west!"
+	done
+
+Route18Gate2F_BinocularsRightScript:
+	jumptext .Text
+.Text:
+	text "Looked into the"
+	line "binoculars."
+
+	para "There are people"
+	line "swimming!"
+	done
+
 Route18Gate2F_MapEvents:
 	def_warp_events
 	warp_event  7,  7, ROUTE_18_GATE_1F, 5
@@ -13,8 +41,8 @@ Route18Gate2F_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  1,  2, BGEVENT_READ, BGEvent
-	bg_event  6,  2, BGEVENT_READ, BGEvent
+	bg_event  1,  2, BGEVENT_READ, Route18Gate2F_BinocularsLeftScript
+	bg_event  6,  2, BGEVENT_READ, Route18Gate2F_BinocularsRightScript
 
 	def_object_events
-	object_event  4,  2, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 4, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  4,  2, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 4, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route18Gate2F_HadenScript, -1
