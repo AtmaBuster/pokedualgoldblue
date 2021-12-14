dc24cb68bb0dfe5f5b23fa2d8137e0903724433a
Atma <ultiomos@gmail.com>
2021-12-13 21:46:23 -0600

    Added event to UndergroundPathRoute6

--- maps/UndergroundPathRoute6.asm
+++ maps/UndergroundPathRoute6.asm
@@ -6,6 +6,14 @@ UndergroundPathRoute6_MapScripts:
 
 	def_callbacks
 
+UndergroundPathRoute6_GirlScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "People often lose"
+	line "things in that"
+	cont "UNDERGROUND PATH."
+	done
+
 UndergroundPathRoute6_MapEvents:
 	def_warp_events
 	warp_event  3,  7, ROUTE_6, 4
@@ -17,4 +25,4 @@ UndergroundPathRoute6_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  2,  3, SPRITE_GIRL, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  2,  3, SPRITE_GIRL, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, UndergroundPathRoute6_GirlScript, -1
