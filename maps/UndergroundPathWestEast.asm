6ad0ce0981b9220aa92996b466c92ae4ae50017e
Atma <ultiomos@gmail.com>
2021-12-13 21:53:35 -0600

    Added events to UndergroundPathWestEast

--- maps/UndergroundPathWestEast.asm
+++ maps/UndergroundPathWestEast.asm
@@ -5,6 +5,12 @@ UndergroundPathWestEast_MapScripts:
 
 	def_callbacks
 
+UndergroundPathWestEast_HiddenNugget:
+	hiddenitem NUGGET, EVENT_UNDERGROUND_PATH_HIDDEN_NUGGET
+
+UndergroundPathWestEast_HiddenElixer:
+	hiddenitem ELIXER, EVENT_UNDERGROUND_PATH_HIDDEN_ELIXER
+
 UndergroundPathWestEast_MapEvents:
 	def_warp_events
 	warp_event  2,  5, UNDERGROUND_PATH_ROUTE_7, 3
@@ -13,5 +19,7 @@ UndergroundPathWestEast_MapEvents:
 	def_coord_events
 
 	def_bg_events
+	bg_event 12,  2, BGEVENT_ITEM, UndergroundPathWestEast_HiddenNugget
+	bg_event 21,  5, BGEVENT_ITEM, UndergroundPathWestEast_HiddenElixer
 
 	def_object_events
