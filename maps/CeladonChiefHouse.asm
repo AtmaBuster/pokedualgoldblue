4dc45d1f09f53811edc92b60d21048d85f45f805
Atma <ultiomos@gmail.com>
2021-12-19 23:03:38 -0600

    Added events to CeladonChiefHouse

--- maps/CeladonChiefHouse.asm
+++ maps/CeladonChiefHouse.asm
@@ -8,6 +8,35 @@ CeladonChiefHouse_MapScripts:
 
 	def_callbacks
 
+CeladonChiefHouse_OldManScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "Hehehe! The slots"
+	line "just reel in the"
+	cont "dough, big time!"
+	done
+
+CeladonChiefHouse_RocketScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "CHIEF!"
+
+	para "We just shipped"
+	line "2000 #MON as"
+	cont "slot prizes!"
+	done
+
+CeladonChiefHouse_SailorScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "Don't touch the"
+	line "poster at the"
+	cont "GAME CORNER!"
+
+	para "There's no secret"
+	line "switch behind it!"
+	done
+
 CeladonChiefHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, CELADON_CITY, 12
@@ -18,6 +47,6 @@ CeladonChiefHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  4,  2, SPRITE_KANTO_GRAMPS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  1,  4, SPRITE_KANTO_ROCKET, SPRITEMOVEDATA_WANDER, 4, 4, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  5,  6, SPRITE_KANTO_SAILOR, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  4,  2, SPRITE_KANTO_GRAMPS, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CeladonChiefHouse_OldManScript, -1
+	object_event  1,  4, SPRITE_KANTO_ROCKET, SPRITEMOVEDATA_WANDER, 4, 4, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CeladonChiefHouse_RocketScript, -1
+	object_event  5,  6, SPRITE_KANTO_SAILOR, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CeladonChiefHouse_SailorScript, -1
