2aeb5eca21c67a94bd1e824b3ef5528b2bddcc07
Atma <ultiomos@gmail.com>
2021-12-19 23:19:13 -0600

    Added events to CeladonMansion2F

--- maps/CeladonMansion2F.asm
+++ maps/CeladonMansion2F.asm
@@ -5,6 +5,16 @@ CeladonMansion2F_MapScripts:
 
 	def_callbacks
 
+CeladonMansion2F_SignScript:
+	jumptext .Text
+.Text:
+	text "GAME FREAK"
+	line "Meeting Room"
+	done
+
+CeladonMansion2F_PCScript:
+	jumpstd PCScript
+
 CeladonMansion2F_MapEvents:
 	def_warp_events
 	warp_event  6,  1, CELADON_MANSION_3F, 1
@@ -15,6 +25,7 @@ CeladonMansion2F_MapEvents:
 	def_coord_events
 
 	def_bg_events
-	bg_event  4,  9, BGEVENT_READ, BGEvent
+	bg_event  4,  9, BGEVENT_READ, CeladonMansion2F_SignScript
+	bg_event  0,  5, BGEVENT_UP, CeladonMansion2F_PCScript
 
 	def_object_events
