74e7638738a2262f0637b6d1222f7a77cb7865a2
Atma <ultiomos@gmail.com>
2021-12-21 21:06:47 -0600

    Added events to FuchsiaBillsGrandpasHouse

--- maps/FuchsiaBillsGrandpasHouse.asm
+++ maps/FuchsiaBillsGrandpasHouse.asm
@@ -8,6 +8,40 @@ FuchsiaBillsGrandpasHouse_MapScripts:
 
 	def_callbacks
 
+FuchsiaBillsGrandpasHouse_WomanScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "SAFARI ZONE's"
+	line "WARDEN is old,"
+	cont "but still active!"
+
+	para "All his teeth are"
+	line "false, though."
+	done
+
+FuchsiaBillsGrandpasHouse_GrandpaScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "Hmm? You've met"
+	line "BILL?"
+
+	para "He's my grandson!"
+
+	para "He always liked"
+	line "collecting things"
+	cont "even as a child!"
+	done
+
+FuchsiaBillsGrandpasHouse_KidScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "BILL files his"
+	line "own #MON data"
+	cont "on his PC!"
+
+	para "Did he show you?"
+	done
+
 FuchsiaBillsGrandpasHouse_MapEvents:
 	def_warp_events
 	warp_event  2,  7, FUCHSIA_CITY, 2
@@ -18,6 +52,6 @@ FuchsiaBillsGrandpasHouse_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  2,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  7,  2, SPRITE_GAMBLER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  5,  5, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  2,  3, SPRITE_POKEFAN_F, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FuchsiaBillsGrandpasHouse_WomanScript, -1
+	object_event  7,  2, SPRITE_GAMBLER, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FuchsiaBillsGrandpasHouse_GrandpaScript, -1
+	object_event  5,  5, SPRITE_KANTO_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FuchsiaBillsGrandpasHouse_KidScript, -1
