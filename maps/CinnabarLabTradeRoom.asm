83f9589f91b84c996275c66546c632c52de80ce6
Atma <ultiomos@gmail.com>
2021-12-22 16:45:05 -0600

    Added events to CinnabarLabTradeRoom

--- maps/CinnabarLabTradeRoom.asm
+++ maps/CinnabarLabTradeRoom.asm
@@ -8,6 +8,34 @@ CinnabarLabTradeRoom_MapScripts:
 
 	def_callbacks
 
+CinnabarLabTradeRoom_FossilGuyScript:
+	jumptextfaceplayer .Text
+.Text:
+	text "I found this very"
+	line "strange fossil in"
+	cont "MT.MOON!"
+
+	para "I think it's a"
+	line "rare, prehistoric"
+	cont "#MON!"
+	done
+
+CinnabarLabTradeRoom_CliftonScript:
+	faceplayer
+	opentext
+	trade NPC_TRADE_CLIFTON
+	waitbutton
+	closetext
+	end
+
+CinnabarLabTradeRoom_NormaScript:
+	faceplayer
+	opentext
+	trade NPC_TRADE_NORMA
+	waitbutton
+	closetext
+	end
+
 CinnabarLabTradeRoom_MapEvents:
 	def_warp_events
 	warp_event  2,  7, CINNABAR_LAB, 3
@@ -18,6 +46,6 @@ CinnabarLabTradeRoom_MapEvents:
 	def_bg_events
 
 	def_object_events
-	object_event  3,  2, SPRITE_KANTO_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  1,  4, SPRITE_KANTO_GRAMPS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
-	object_event  5,  5, SPRITE_KANTO_BEAUTY, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, -1
+	object_event  3,  2, SPRITE_KANTO_SUPER_NERD, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarLabTradeRoom_FossilGuyScript, -1
+	object_event  1,  4, SPRITE_KANTO_GRAMPS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarLabTradeRoom_CliftonScript, -1
+	object_event  5,  5, SPRITE_KANTO_BEAUTY, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CinnabarLabTradeRoom_NormaScript, -1
