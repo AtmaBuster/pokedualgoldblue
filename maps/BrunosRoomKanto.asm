fb666bd10d8f0b393fd2e197a854b6e68dcbeef7
Atma <ultiomos@gmail.com>
2021-12-24 01:06:27 -0600

    Added events to BrunosRoomKanto

--- maps/BrunosRoomKanto.asm
+++ maps/BrunosRoomKanto.asm
@@ -3,8 +3,109 @@
 
 BrunosRoomKanto_MapScripts:
 	def_scene_scripts
+	scene_script .ForceUp ; SCENE_DEFAULT
+	scene_script DummyScript ; SCENE_FINISHED
 
 	def_callbacks
+	callback MAPCALLBACK_TILES, .Doors
+
+.ForceUp:
+	sdefer .ForceUpScript
+	end
+
+.Doors:
+	checkevent EVENT_BRUNOS_ROOM_KANTO_EXIT_OPEN
+	iffalse .KeepClosed
+	changeblock 4, 0, $74 ; open door
+.KeepClosed:
+	endcallback
+
+.ForceUpScript:
+	applymovement PLAYER, .UpMove
+	setscene SCENE_FINISHED
+	end
+
+.UpMove:
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step_end
+
+BrunosRoomKanto_BrunoScript:
+	faceplayer
+	opentext
+	checkevent EVENT_BEAT_BRUNO_GEN1
+	iftrue .FightDone
+	writetext .IntroText
+	waitbutton
+	closetext
+	winlosstext .WinText, 0
+	loadtrainer BRUNO_GEN1, BRUNO1_GEN1
+	startbattle
+	reloadmapafterbattle
+	setevent EVENT_BEAT_BRUNO_GEN1
+	opentext
+.FightDone:
+	writetext .AfterText
+	waitbutton
+	changeblock 4, 0, $74 ; open door
+	reloadmappart
+	setevent EVENT_BRUNOS_ROOM_KANTO_EXIT_OPEN
+	closetext
+	end
+
+.IntroText:
+	text "I am BRUNO of"
+	line "the ELITE FOUR!"
+
+	para "Through rigorous"
+	line "training, people"
+	cont "and #MON can"
+	cont "become stronger!"
+
+	para "I've weight"
+	line "trained with"
+	cont "my #MON!"
+
+	para "<PLAYER>!"
+
+	para "We will grind you"
+	line "down with our"
+	cont "superior power!"
+
+	para "Hoo hah!"
+	done
+
+.WinText:
+	text "Why?"
+	line "How could I lose?"
+	done
+
+.AfterText:
+	text "My job is done!"
+	line "Go face your next"
+	cont "challenge!"
+	done
+
+BrunosRoomKanto_ForceIntoRoomScript:
+	opentext
+	writetext .Text
+	waitbutton
+	closetext
+	applymovement PLAYER, .MoveUp
+	end
+
+.Text:
+	text "Someone's voice:"
+	line "Don't run away!"
+	done
+
+.MoveUp:
+	step UP
+	step_end
 
 BrunosRoomKanto_MapEvents:
 	def_warp_events
@@ -14,8 +115,10 @@ BrunosRoomKanto_MapEvents:
 	warp_event  5,  0, AGATHAS_ROOM, 2
 
 	def_coord_events
+	coord_event  4, 10, SCENE_FINISHED, BrunosRoomKanto_ForceIntoRoomScript
+	coord_event  5, 10, SCENE_FINISHED, BrunosRoomKanto_ForceIntoRoomScript
 
 	def_bg_events
 
 	def_object_events
-	object_event  5,  2, SPRITE_BRUNO, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, ObjectEvent, -1
+	object_event  5,  2, SPRITE_BRUNO, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 1, BrunosRoomKanto_BrunoScript, -1
