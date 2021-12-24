4d19b2f0181f516ecb5afd91dc863fd495a0cc69
Atma <ultiomos@gmail.com>
2021-12-24 01:36:00 -0600

    Added events to HallOfFameKanto

--- maps/HallOfFameKanto.asm
+++ maps/HallOfFameKanto.asm
@@ -3,8 +3,62 @@
 
 HallOfFameKanto_MapScripts:
 	def_scene_scripts
+	scene_script .Enter ; SCENE_DEFAULT
 
 	def_callbacks
+
+.Enter:
+	sdefer .EnterScript
+	end
+
+.EnterScript:
+	applymovement PLAYER, .Approach
+	turnobject PLAYER, RIGHT
+	turnobject HALLOFFAMEKANTO_OAK, LEFT
+	opentext
+	writetext .Text
+	waitbutton
+	setevent EVENT_BEAT_CHAMPION_GEN1
+	setevent EVENT_BEAT_LEAGUE
+	halloffame
+	end
+
+.Text:
+	text "OAK: Er-hem!"
+	line "Congratulations"
+	cont "<PLAYER>!"
+
+	para "This floor is the"
+	line "#MON HALL OF"
+	cont "FAME!"
+
+	para "#MON LEAGUE"
+	line "champions are"
+	cont "honored for their"
+	cont "exploits here!"
+
+	para "Their #MON are"
+	line "also recorded in"
+	cont "the HALL OF FAME!"
+
+	para "<PLAYER>! You have"
+	line "endeavored hard"
+	cont "to become the new"
+	cont "LEAGUE champion!"
+
+	para "Congratulations,"
+	line "<PLAYER>, you and"
+	cont "your #MON are"
+	cont "HALL OF FAMERs!"
+	done
+
+.Approach:
+	step UP
+	step UP
+	step UP
+	step UP
+	step UP
+	step_end
 
 HallOfFameKanto_MapEvents:
 	def_warp_events
